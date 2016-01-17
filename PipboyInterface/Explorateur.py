3359ea8573b6b396baeb397790b4254b1c73a8ac
romsala <romlover.sala@gmail.com>
2016-01-17 13:56:59 +0100

    Fonction pour récuperer le contenu d'un dossier

--- PipboyInterface/Explorateur.py
+++ PipboyInterface/Explorateur.py
@@ -1,10 +1,23 @@
 import pygame
 from pygame.locals import*
+import os
 
 pygame.init()
+
+liste_fichiers=[]
+
 
 def InitExplorateur(fenetre):
     fenetre.fill((0, 0, 0))
     top = pygame.image.load("Interface/Explorateur/Explorateur-top.png")
     fenetre.blit(top, (0, 0))
     pygame.display.flip()
+
+def GetListeFichiers(path):
+    global liste_fichiers
+    liste_fichiers = []
+    for file in os.listdir(path):
+        liste_fichiers.append(file)
+    print(liste_fichiers)
+
+
